73ca1138c997a0efe398b14352233b296beecd9c
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-12-20 21:16:47 +0100

    Rendering images given a list of messages based on the java Graphics2D api

--- src/main/java/com/karumi/reddo/view/MatrixLedView.java
+++ src/main/java/com/karumi/reddo/view/MatrixLedView.java
@@ -16,9 +16,14 @@
 
 package com.karumi.reddo.view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class MatrixLedView implements View {
+
+  public static final int LED_HEIGHT = 32;
 
   private final int fps;
 
@@ -27,7 +32,26 @@ public class MatrixLedView implements View {
   }
 
   @Override public void showMessages(List<String> messages) {
-
+    String joinMessages = String.join(" --- ", messages);
+    int stringWidthInPixels = getStringWidthInPixels(joinMessages);
+    BufferedImage outputImage = new BufferedImage(stringWidthInPixels, LED_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    Graphics2D graphics = outputImage.createGraphics();
+    graphics.setFont(getFont());
+    graphics.setColor(Color.WHITE);
+    graphics.drawString(joinMessages,0,LED_HEIGHT);
+    JOptionPane.showMessageDialog(null, new ImageIcon(outputImage));
   }
+
+  private int getStringWidthInPixels(String joinMessages) {
+    Graphics graphics = new BufferedImage(1,1,BufferedImage.TYPE_INT_RGB).getGraphics();
+    Font font = getFont();
+    graphics.setFont(font);
+    return graphics.getFontMetrics().stringWidth(joinMessages);
+  }
+
+  private Font getFont() {
+    return new Font("Serif", Font.PLAIN, LED_HEIGHT);
+  }
+
 
 }
