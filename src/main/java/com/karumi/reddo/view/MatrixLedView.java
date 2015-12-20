069a72efd24f326f27fcdf6fa3b92fd7c9a8e932
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-12-20 22:00:13 +0100

    Apply project checkstyle to the two classes where we've been working on

--- src/main/java/com/karumi/reddo/view/MatrixLedView.java
+++ src/main/java/com/karumi/reddo/view/MatrixLedView.java
@@ -16,71 +16,73 @@
 
 package com.karumi.reddo.view;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MatrixLedView implements View {
 
-    public static final int LED_HEIGHT = 32;
+  public static final int LED_HEIGHT = 32;
 
-    private final int fps;
+  private final int fps;
 
-    public MatrixLedView(int fps) {
-        this.fps = fps;
+  public MatrixLedView(int fps) {
+    this.fps = fps;
+  }
+
+  @Override public void showMessages(List<String> messages) {
+    String joinMessages = String.join(" --- ", messages);
+    int stringWidthInPixels = getStringWidthInPixels(joinMessages);
+    BufferedImage outputImage = getJoinMessagesAsImage(joinMessages, stringWidthInPixels);
+    drawImage(outputImage);
+  }
+
+  private void drawImage(BufferedImage outputImage) {
+    int width = outputImage.getWidth();
+    JFrame frame = new JFrame();
+    JPanel comp = new JPanel();
+    frame.add(comp);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(16, 32 + 22);
+    frame.setVisible(true);
+    for (int i = 0; i < width; i++) {
+      frame.getGraphics().drawImage(outputImage, -i, 22, null);
+      waitForNextFrame();
     }
+  }
 
-    @Override
-    public void showMessages(List<String> messages) {
-        String joinMessages = String.join(" --- ", messages);
-        int stringWidthInPixels = getStringWidthInPixels(joinMessages);
-        BufferedImage outputImage = getJoinMessagesAsImage(joinMessages, stringWidthInPixels);
-        drawImage(outputImage);
+  private void waitForNextFrame() {
+    float frameTime = 1 / (float) fps * 1000;
+    try {
+      Thread.sleep((long) frameTime);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    private void drawImage(BufferedImage outputImage) {
-        int width = outputImage.getWidth();
-        JFrame frame = new JFrame();
-        JPanel comp = new JPanel();
-        frame.add(comp);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(16, 32 + 22);
-        frame.setVisible(true);
-        for (int i = 0; i < width; i++) {
-            frame.getGraphics().drawImage(outputImage, -i, 22, null);
-            waitForNextFrame();
-        }
-    }
+  private BufferedImage getJoinMessagesAsImage(String joinMessages, int stringWidthInPixels) {
+    BufferedImage outputImage =
+        new BufferedImage(stringWidthInPixels, LED_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    Graphics2D graphics = outputImage.createGraphics();
+    graphics.setFont(getFont());
+    graphics.setColor(Color.BLUE);
+    graphics.drawString(joinMessages, 0, LED_HEIGHT - 5);
+    return outputImage;
+  }
 
-    private void waitForNextFrame() {
-        float frameTime = 1 / (float) fps * 1000;
-        try {
-            Thread.sleep((long) frameTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+  private int getStringWidthInPixels(String joinMessages) {
+    Graphics graphics = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB).getGraphics();
+    Font font = getFont();
+    graphics.setFont(font);
+    return graphics.getFontMetrics().stringWidth(joinMessages);
+  }
 
-    private BufferedImage getJoinMessagesAsImage(String joinMessages, int stringWidthInPixels) {
-        BufferedImage outputImage = new BufferedImage(stringWidthInPixels, LED_HEIGHT, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics = outputImage.createGraphics();
-        graphics.setFont(getFont());
-        graphics.setColor(Color.BLUE);
-        graphics.drawString(joinMessages, 0, LED_HEIGHT - 5);
-        return outputImage;
-    }
-
-    private int getStringWidthInPixels(String joinMessages) {
-        Graphics graphics = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB).getGraphics();
-        Font font = getFont();
-        graphics.setFont(font);
-        return graphics.getFontMetrics().stringWidth(joinMessages);
-    }
-
-    private Font getFont() {
-        return new Font("Serif", Font.PLAIN, LED_HEIGHT - 2);
-    }
-
-
+  private Font getFont() {
+    return new Font("Serif", Font.PLAIN, LED_HEIGHT - 2);
+  }
 }
