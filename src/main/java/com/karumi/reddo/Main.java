b47078f3317cbe8a540b5b9a493d8896ef428f72
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-12-20 21:57:48 +0100

    Transform Main class execution into an ifinite loop

--- src/main/java/com/karumi/reddo/Main.java
+++ src/main/java/com/karumi/reddo/Main.java
@@ -20,16 +20,19 @@ import com.karumi.reddo.config.ReddoConfig;
 import com.karumi.reddo.config.TypesafeHubReddoConfig;
 import com.karumi.reddo.task.ReddoTask;
 import com.karumi.reddo.view.View;
+
 import java.util.Collection;
 
 public class Main {
 
-  public static void main(String[] args) {
-    ReddoConfig config = new TypesafeHubReddoConfig();
-    View view = config.getView();
-    Reddo reddo = new Reddo(view);
-    Collection<ReddoTask> tasksFromConfig = config.getTasks();
-    reddo.addTasks(tasksFromConfig);
-    reddo.evaluateTasks();
-  }
+    public static void main(String[] args) {
+        ReddoConfig config = new TypesafeHubReddoConfig();
+        View view = config.getView();
+        Reddo reddo = new Reddo(view);
+        Collection<ReddoTask> tasksFromConfig = config.getTasks();
+        reddo.addTasks(tasksFromConfig);
+        while (true) {
+            reddo.evaluateTasks();
+        }
+    }
 }
